6dd42b3f5cff5941a25c0ca80dc5836105fcf72e
Giovanni Ricca <giovanniricca@proton.me>
2024-12-18 13:49:49 +0100

    rubens: Move sensor hals config to vendor
    Change-Id: I792af01129ca41b1c61d5ed033d02105a9dd438a

--- extract-files.py
+++ extract-files.py
@@ -29,7 +29,8 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/etc/sensors/hals.conf': blob_fixup()
-        .regex_replace('android.hardware.sensors@2.X-subhal-mediatek.so', 'android.hardware.sensors@2.0-subhal-impl-1.0.so'),
+        .regex_replace('android.hardware.sensors@2.X-subhal-mediatek.so', 'android.hardware.sensors@2.0-subhal-impl-1.0.so')
+        .regex_replace('sensors.touch.detect.so', 'sensors.dynamic_sensor_hal.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libalsautils.so', 'libalsautils-v31.so'),
