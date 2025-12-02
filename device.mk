c0805da5a23b7b0f05add6c594f874e058b29591
mmqm <3227495621@qq.com>
2025-12-03 00:32:32 +0800

    rubens: Add SKU props * Set USB product string * Set bluetooth device name

--- device.mk
+++ device.mk
@@ -55,6 +55,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Sku properties
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/sku/,$(TARGET_COPY_OUT_ODM)/etc)
+
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 31
 
