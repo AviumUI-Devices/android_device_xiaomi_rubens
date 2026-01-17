ProjectRubens/device_xiaomi_rubens

rubens: AviumUI-ify

avium_common.mk
@@ -1,0 +1,12 @@
+AVIUM_BUILDTYPE := Unoffical
+AVIUM_VERSION_APPEND_TIME_OF_DAY := false
+
+# GMS
+WITH_GMS := true
+TARGET_GMS_TYPE := MINI
+
+# Google IMS
+TARGET_INCLUDE_GOOGLEIME := true
+TARGET_GOOGLEIME_OVERRIDE_IME := true
+
+AVIUM_MAINTAINER := InvalidName

AviumUI-Devices/android_device_xiaomi_rubens

avium_common.mk
@@ -1,0 +1,12 @@
+AVIUM_BUILDTYPE := Unoffical
+AVIUM_VERSION_APPEND_TIME_OF_DAY := false
+
+# GMS
+WITH_GMS := true
+TARGET_GMS_TYPE := FULL
+
+# Google IMS
+TARGET_INCLUDE_GOOGLEIME := true
+TARGET_GOOGLEIME_OVERRIDE_IME := true
+
+AVIUM_MAINTAINER := InvalidName